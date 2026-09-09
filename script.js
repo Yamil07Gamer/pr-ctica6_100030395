f0bb1ce3dda29a9453f8da50f068add84f17fb70
Yamil07Gamer <yalexeicparedes@gmail.com>
2026-09-08 23:22:53 -0600

    Funciones adicionales (no no im from egland americaaaaa)

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', function playAudio() {
+        const audio = document.getElementById('musicaFondo');
+        audio.volume = 0.2; // Volumen al 40%
+        audio.play();
+        
+        // Se remueve el evento para que solo se ejecute con el primer clic
+        document.removeEventListener('click', playAudio);
+    }, { once: true });
+
+document.querySelectorAll('.btn').forEach(boton => {
+        boton.addEventListener('click', () => {
+            const audioClick = document.getElementById('sonidoClick');
+            audioClick.currentTime = 0; // Reinicia el audio para permitir clics rápidos seguidos
+            audioClick.volume = 0.3;    // Volumen del clic
+            audioClick.play().catch(error => console.log("Reproducción bloqueada temporalmente"));
+        });
+    });
